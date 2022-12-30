eb22418cebec4c22020b2843893fba86fdd3dfc4
Roshni V R <vrroshni2001@gmail.com>
2022-12-31 00:26:26 +0530

    login and sign up tested

--- server/main.py
+++ server/main.py
@@ -1,26 +1,51 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+# pip install "uvicorn[standard]"
+
 from pydantic import BaseModel
 from typing import Union
+
 import requests
 
-
-app=FastAPI()
+app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credenttials=True,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-PROJECT_ID=""
-PRIVATE_KEY=""
-
-
+PROJECT_ID="5c1fa8b5-3396-4ca5-b9c9-8a71a2fcb221"
+PRIVATE_KEY="b289d28b-31d3-4a20-a613-b5c5702c8e5e"
 class User(BaseModel):
     username: str
     secret: str
     email: Union[str, None] = None
     first_name: Union[str, None] = None
     last_name: Union[str, None] = None
+
+@app.post('/login/')
+async def root(user: User):
+    response = requests.get('https://api.chatengine.io/users/me/', 
+        headers={ 
+            "Project-ID": PROJECT_ID,
+            "User-Name": user.username,
+            "User-Secret": user.secret
+        }
+    )
+    return response.json()
+
+@app.post('/signup/')
+async def root(user: User):
+    response = requests.post('https://api.chatengine.io/users/', 
+        data={
+            "username": user.username,
+            "secret": user.secret,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+        },
+        headers={ "Private-Key": PRIVATE_KEY }
+    )
+    return response.json()
